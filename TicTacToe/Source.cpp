1a47547d839eb236c86e659b07a8043cc3a3ca75
Petter <setterberg.p@gmail.com>
2023-08-08 17:32:30 +0200

    created board and start message

--- TicTacToe/Source.cpp
+++ TicTacToe/Source.cpp
@@ -1,19 +1,38 @@
 #include <iostream>
-using namespace std;
+using namespace std;	
 
-void draw(char grid[]);
+
+void message();
+void draw(char board[]);
 
 int main()
 {
 	const int size = 9;
-	char grid[size] = { '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-
-	draw(grid);
+	char board[] = { '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+	
+	message();
+	draw(board);
 	
 	system("pause");
 }
 
-void draw(char grid[])
+void message()
 {
-	cout << grid << endl;
+	cout << "TIC TAC TOE" << endl;
+	cout << "Player 1 = X" << endl;
+	cout << "Player 2 = O" << endl;
+	cout << endl;
+}
+
+void draw(char board[])
+{
+	cout << "\t" << board[0] << "\t" << "|" << "\t" << board[1] << "\t" << "|" << "\t" << board[2] << endl;
+	cout << "     -----------|---------------|-----------" << endl;
+
+	cout << "\t" << board[3] << "\t" << "|" << "\t" << board[4] << "\t" << "|" << "\t" << board[5] << endl;
+	cout << "     -----------|---------------|-----------" << endl;
+
+	cout << "\t" << board[6] << "\t" << "|" << "\t" << board[7] << "\t" << "|" << "\t" << board[8] << endl;
+
+	cout << endl;
 }
